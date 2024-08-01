7dc85ae001b6e20a8d378d5005dbc39d7e646e2c
Natnicha♥ <142340818+Natnicha2803@users.noreply.github.com>
2024-08-01 23:12:50 +0700

    Create if_else.cpp

---
+++ Assignment1/if_else.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 3 ;
+    int b = 4 ;
+    if( a > b ) {
+        printf( "The number in A is greater than B. \n" ) ;
+    } else {
+        printf( "The number in A is greater than A. \n" ) ;
+    }//end if
+    
+    return 0 ;
+}//end main
